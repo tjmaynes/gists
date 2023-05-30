2c86f73219d537d927bf51774f5ab47935185edc
TJ Maynes <tj@tjmaynes.com>
2023-05-30 09:32:04 -0500

    chore(c#): move some csharp projects into their own repos

--- c#/sample-csharp-app/src/ShoppingService.Api/ApplicationContext.cs
+++
@@ -1,9 +0,0 @@
-using Microsoft.EntityFrameworkCore;
-using ShoppingService.Core.Cart;
-
-namespace ShoppingService.Api {
-    public class ApplicationContext: DbContext
-    {
-        public DbSet<CartItem> CartItems { get; set; }
-    }
-}
--- c#/sample-csharp-app/tests/ShoppingService.Core.Tests/Data/CartItemValidatorFixture.cs
+++
@@ -1,8 +0,0 @@
-using ShoppingService.Core.Cart;
-using FluentValidation;
-
-namespace ShoppingService.Core.Tests.Data {
-    public class CartItemValidatorFixture {
-        public static AbstractValidator<CartItem> Initialize() => new CartItemValidator();
-    }
-}
