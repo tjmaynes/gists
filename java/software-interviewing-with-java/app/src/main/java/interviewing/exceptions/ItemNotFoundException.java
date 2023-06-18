9722692b4d3884f746dbf2082fd8e3b05a8e938b
TJ Maynes <tj@tjmaynes.com>
2023-06-18 17:18:38 -0500

    chore(java): remove software-interviewing-with-java

--- java/software-interviewing-with-java/app/src/main/java/interviewing/exceptions/ItemNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package interviewing.exceptions;
-
-public class ItemNotFoundException extends RuntimeException {
-    public ItemNotFoundException(String errorMessage, Throwable err) {
-        super(errorMessage, err);
-    }
-}
